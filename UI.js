825741e29aed5df96a331deb7777fb9607e4b884
yubin <cyoobin@naver.com>
2024-02-22 11:04:55 +0900

    css 문제 수정

--- UI.js
+++ UI.js
@@ -6,6 +6,8 @@ export default class UI {
   }
 
   displayProfile(profile) {
+    const memberSince = profile.created_at.slice(0, 10);
+
     return `<div class="profile-container card mb-4">
     <div class="card-body d-flex p-3 gap-3">
       <div class="profile-left d-flex flex-column me-2">
@@ -14,9 +16,10 @@ export default class UI {
           src="${profile.avatar_url}"
           alt="샘플이미지"
         />
-        <button type="button" class="btn view-btn btn-primary mt-2">
+        <a href="${profile.html_url}" target="_blank" class="d-block">
+        <button type="button" class="btn d-block w-100 view-btn btn-primary mt-2">
           View Profile
-        </button>
+        </button></a>
       </div>
       <div class="profile-right d-flex flex-column flex-grow-1">
         <div class="profile-badges d-flex gap-1 mb-3">
@@ -31,18 +34,28 @@ export default class UI {
         </div>
         <ul class="profile-info-list list-group w-100">
           <ul class="list-group-item">
-            Company: ${profile.company}
-          </ul>
-          <ul class="list-group-item">
-            Website/Blog: ${
-              profile.blog == "" ? "There is no blog " : profile.blog
+            Company: ${
+              profile.company !== null
+                ? profile.company
+                : "There is no company..."
             }
           </ul>
           <ul class="list-group-item">
-            Location: ${profile.location}
+            Website/Blog: 
+            <a class="blog-link-text" href="${
+              profile.blog != "" ? profile.blog : "#"
+            }"/>${profile.blog == "" ? "There is no blog..." : profile.blog}
+            </a>
           </ul>
           <ul class="list-group-item">
-            Member Since: ${profile.create_at}
+            Location: ${
+              profile.location == ""
+                ? "There is no location..."
+                : profile.location
+            }
+          </ul>
+          <ul class="list-group-item">
+            Member Since: ${memberSince}
           </ul>
         </ul>
       </div>
@@ -51,17 +64,17 @@ export default class UI {
   }
 
   displayRepos(repos) {
-    repos = repos.slice(0, 7); // 최근 레포지토리 6개만 보여준다.
+    repos = repos.slice(0, 6); // 최근 레포지토리 6개만 보여준다.
     const repoInfoArray = repos.map((repo) => {
       return `<div class="repository-card card mb-3">
       <div class="card-body d-flex">
-        <a href="${repo.url}" class="flex-grow-1 card-link">${repo.name}</a>
+        <a href="${repo.html_url}" target="_blank" class="repository-name flex-grow-1 card-link">${repo.name}</a>
         <div class="repository-badges d-flex gap-1 flex-grow-1">
           <span class="badge bg-primary">Stars: ${repo.stargazers_count}</span>
           <span class="badge bg-success">Watchers: ${repo.watchers_count}</span>
           <span class="badge bg-dark">Forks: ${repo.forks_count}</span>
         </div>
-      </div>`;
+      </div></div>`;
     });
 
     const repoCards = repoInfoArray.join("");
